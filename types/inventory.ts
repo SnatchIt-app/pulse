5ae90f2f050a1b0b3ee6101c197bfd3e816f3299
JOSE TASCON <gnvprod@gmail.com>
2026-05-27 13:50:17 -0400

    chore: add normalized inventory structure

---
+++ types/inventory.ts
@@ -0,0 +1,38 @@
+export type LuxuryTier = 'ultra-luxury';
+
+export interface Car {
+  slug: string;
+  make: string;
+  model: string;
+  exterior_color: string;
+  interior_color: string | null;
+  color_label: string;
+  body_style?: string;
+  luxury_tier: LuxuryTier;
+  homepage_worthy: boolean;
+  needs_review?: boolean;
+  quote_only: true;
+}
+
+export interface CarIndex {
+  generated: string;
+  count: number;
+  vehicles: Car[];
+}
+
+export interface Yacht {
+  slug: string;
+  make: string;
+  model?: string;
+  name?: string;
+  length_ft?: number;
+  source_tag?: string;
+  quote_only: true;
+  asset_count: number;
+}
+
+export interface YachtIndex {
+  generated: string;
+  count: number;
+  yachts: Yacht[];
+}
